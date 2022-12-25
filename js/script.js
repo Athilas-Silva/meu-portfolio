d467604b0e6951af10860efb4f0b9d41a0f84f76
Athilas Silva <athilas.soares@hotmail.com>
2022-12-25 00:45:25 -0300

    update
    Adicionando arquivo json com dados dos projetos

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // MENU
-const menuSection = document.querySelector(".menu-section")
-const menuToggle = document.querySelectorAll(".menu-toggle")
+const menuSection = document.querySelector(".menu-section");
+const menuToggle = document.querySelectorAll(".menu-toggle");
 
 for(const element of menuToggle){
     element.addEventListener("click", () => {
@@ -23,14 +23,41 @@ const scrollReveal = ScrollReveal({
     distance: "30px",
     duration: 700,
     reset: false,
-})
+});
 
 //Ordem de apresentação
 scrollReveal.reveal(
     `header img, .container .menu-section,
     .presentation .container, .presentation div,
     .skill h3, .cards .card,
-    main h3, .cards .card,
+    main h3, #projects .cards,
     footer p, footer a
     `, { interval: 100} 
-)
+);
+
+const textProject = document.querySelector("#projects .cards");
+
+fetch("projects.json").then((res) => {
+    res.json().then((dados) => {
+        dados.projects.map((project) => {
+            textProject.innerHTML += 
+            `
+                <div class="card">
+                    <h2>${project.name}</h2>
+                    <div class="image">
+                        <img src="${project.img}" alt="Poster da aplicação">
+                    </div>
+
+                    <div class="content">
+                        <p class="title text--medium">
+                            ${project.description}
+                        </p>
+                        <div class="info">
+                            <a class="button" href="${project.link}" target="_blank">Ver projeto</a>
+                        </div>
+                    </div>
+                </div>
+            `;
+        })
+    });
+});
